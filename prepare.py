b2a6dfcb0e4a83e422e498c80e24ea9666f94b9d
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-06-19 23:36:05 -0500

    prepare funcs

--- prepare.py
+++ prepare.py
@@ -4,12 +4,22 @@ import numpy as np
 from datetime import date
 import matplotlib.pyplot as plt
 import math
-
+import sklearn
 from sklearn import metrics
 import sklearn.preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.cluster import KMeans
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+from sklearn.preprocessing import PolynomialFeatures, RobustScaler
+from statsmodels.formula.api import ols
+from math import sqrt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import median_absolute_error, r2_score, mean_absolute_error, mean_squared_error
+import matplotlib.pyplot as plt
 
 
 
